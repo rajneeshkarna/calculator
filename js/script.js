6de2f0ab899d890ea0761c4ee653a99557c6c678
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-19 20:34:44 +0530

    add event listener to show in display

--- js/script.js
+++ js/script.js
@@ -33,3 +33,66 @@ function operate(operator, firstNumber, secondNumber) {
       return divide(firstNumber, secondNumber)
   }
 }
+
+let displayValue = '';
+
+function updateDisplay() {
+    document.querySelector('.display').textContent = displayValue;
+}
+
+document.getElementById('btn-1').addEventListener('click', function() {
+    displayValue += '1';
+    updateDisplay();
+});
+
+document.getElementById('btn-2').addEventListener('click', function() {
+    displayValue += '2';
+    updateDisplay();
+});
+
+document.getElementById('btn-3').addEventListener('click', function() {
+    displayValue += '3';
+    updateDisplay();
+});
+
+document.getElementById('btn-4').addEventListener('click', function() {
+    displayValue += '4';
+    updateDisplay();
+});
+
+document.getElementById('btn-5').addEventListener('click', function() {
+  displayValue += '5';
+  updateDisplay();
+});
+
+document.getElementById('btn-6').addEventListener('click', function() {
+  displayValue += '6';
+  updateDisplay();
+});
+
+document.getElementById('btn-7').addEventListener('click', function() {
+  displayValue += '7';
+  updateDisplay();
+});
+
+document.getElementById('btn-8').addEventListener('click', function() {
+  displayValue += '8';
+  updateDisplay();
+});
+
+document.getElementById('btn-9').addEventListener('click', function() {
+  displayValue += '9';
+  updateDisplay();
+});
+
+document.getElementById('btn-0').addEventListener('click', function() {
+  displayValue += '0';
+  updateDisplay();
+});
+
+document.querySelector('.clear-btn').addEventListener('click', function() {
+    displayValue = ''; 
+    updateDisplay();
+});
+
+updateDisplay();
